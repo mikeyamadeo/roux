91918dafa192f3eba2c416cf87f5e7c307127bdb
Mikey Murphy <mikeyamadeo@gmail.com>
2018-01-14 12:15:43 -0700

    change way we are checking for window

--- src/UI/packages/global.js
+++ src/UI/packages/global.js
@@ -3,7 +3,7 @@ import { baseFontSize, fonts } from './settings'
 
 css.global('html', { fontSize: baseFontSize, fontFamily: fonts.primary })
 
-if (window) {
+if (typeof window !== `undefined`) {
   const WebFont = require('webfontloader')
   WebFont.load({
     google: { families: [ 'Playfair+Display:400,700', 'serif' ] }
