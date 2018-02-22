18479681e41c9024f994ef4b7067f7c4966f68c2
Mikey Murphy <mikeyamadeo@gmail.com>
2018-02-21 21:05:02 -0700

    use universal version of react-stripe-elements

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { StripeProvider } from 'react-stripe-elements'
+import { StripeProvider } from 'react-stripe-elements-universal'
 import { configureStore } from './src/state/'
 
 exports.replaceRouterComponent = ({ history }) => {
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { StripeProvider } from 'react-stripe-elements'
+import { StripeProvider } from 'react-stripe-elements-universal'
 import { renderToString } from 'react-dom/server'
 import { configureStore } from './src/state/'
 
